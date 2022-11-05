c7c94b714550f1ca085dfb6ebc814f7635bc09c4
Andy13K <aaquinoe1@miumg.edu.gt>
2022-11-05 08:58:50 -0600

    Andy Aquino 0909-22-1669

---
+++ jsparcialfinal.js
@@ -0,0 +1,13 @@
+function calcular (){
+
+    var x = parseInt(document.getElementById('supervisor' 'secretaria' 'digitador' 'mensajero').value);
+    var y = 250
+    var z = parseInt(document.getElementById('horasextras').value);
+    ( x + y + z*18.50);
+    alert("Su salario es") ( x + y + z*18.50);
+    return false
+  }
+
+function alert (){
+    alert("Aca tendria de salir el salario :( " );
+}
